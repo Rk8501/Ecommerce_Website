c1640ea8fbf489efb616ee6e2ef8be0be5bb06f2
Riddhi Koradiya <42riddhikoradiya@gmail.com>
2021-07-26 17:59:19 +0530

    Ecommerce Website Code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceProject.Models;
+
+namespace EcommerceProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        AdminContext db = new AdminContext();
+        public ActionResult Home(string searching)
+        {
+            return View(db.CategoryTable.Where(x => x.Name.Contains(searching) || searching == null).ToList());
+        }
+       
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+    }
+}
